be4e57229db5b55e08fc5e4a47114a52d18e7e1f
Chrisd319 <chrisd319@gmail.com>
2018-01-27 14:35:38 -0500

    test

--- src/main/java/placeholder.java
+++ src/main/java/placeholder.java
@@ -1,5 +1,5 @@
 public class placeholder {
     public static void main(String[] args) {
-        System.out.println("Hello PlaceHolder!");
+        System.out.println("Hello Brick Hack!");
     }
 }
